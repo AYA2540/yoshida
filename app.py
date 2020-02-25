0075640bb71e840374ab183202a464756785b41d
aya <r7.sk52@gmail.com>
2020-02-25 14:46:28 +0900

    パス変更４

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def user_list():
     return render_template("list.html" , user_list = user_list)
 
 # 入店ボタンを押した時
-@app.route("/a87fhmhnkb0t/<int:id>", methods=["POST"])
+@app.route("/a87fhmhnkb0tenter_yes/<int:id>", methods=["POST"])
 def enter_yes(id):
     guest = request.form.get("number")
     enter_time = datetime.now().strftime("%m/%d %H:%M")
@@ -38,10 +38,10 @@ def enter_yes(id):
     c.execute("update users set flag = 1 where id = %s" ,(id,))
     conn.commit()
     conn.close()
-    return redirect("/wRqZejFP_8X")
+    return redirect("/berb7kjfp5v6")
 
 # 退店ボタンを押した時
-@app.route("/hiw5lfsmmj3z/<int:id>")
+@app.route("/hiw5lfsmmj3zout_yes/<int:id>")
 def out_yes(id):
     out_time = datetime.now().strftime("%m/%d/ %H:%M")
     # conn = mysql.connector.connect(user='root',password='A7RmwDLh-Uci',host='127.0.0.1',database='yoshida')
@@ -51,14 +51,14 @@ def out_yes(id):
     c.execute("update times set out_time = %s where user_id = %s and out_time is null" ,(out_time ,id))
     conn.commit()
     conn.close()
-    return redirect("/wRqZejFP_8X")
+    return redirect("/berb7kjfp5v6")
 
 # ログイン
-@app.route("/apyaig9e2qfx", methods=["GET","POST"])
+@app.route("/apyaig9e2qfxlogin", methods=["GET","POST"])
 def login():
     if request.method == "GET":
         if "user_id" in session:
-            return redirect("/SwWn#*CW*z/,")
+            return redirect("/mr0rqm9otr8jenter")
         else:
             return render_template("login.html")
     else:
@@ -89,12 +89,12 @@ def login():
                 return render_template("login.html")
             else:
                 session['user_id'] = user_id[0]
-                return redirect("/SwWn#*CW*z/,")
+                return redirect("/mr0rqm9otr8jenter")
         else:
             return render_template("login.html")
 
 # 入店状況確認ページ
-@app.route("/mr0rqm9otr8j")
+@app.route("/mr0rqm9otr8jenter")
 def enter():
     if "user_id" in session:
         # conn = mysql.connector.connect(user='root',password='A7RmwDLh-Uci',host='127.0.0.1',database='yoshida')
@@ -123,18 +123,18 @@ def enter():
         conn.close()
         return render_template("enter.html" , user_list = user_list , member = member , user_id = user_id)
     else:
-        return redirect("/HBZT,+FQhi*")
+        return redirect("/apyaig9e2qfxlogin")
 
-@app.route("/ssqh6pvqj6ba" ,methods=["GET"])
+@app.route("/ssqh6pvqj6bawelcome" ,methods=["GET"])
 def welcome():
     return render_template("welcome.html")
 
-@app.route("/ixm7uuz0anrn" ,methods=["GET"])
+@app.route("/ixm7uuz0anrnthanks" ,methods=["GET"])
 def thanks():
     return render_template("thanks.html")
 
 # オーナーページ
-@app.route("/u2rdvj15g8q6")
+@app.route("/u2rdvj15g8q6owner")
 def owner():
     if "user_id" in session:
         user_id = session["user_id"]
@@ -153,12 +153,12 @@ def owner():
             conn.close()
             return render_template("owner.html" , user_list = user_list , member = member)
         else:
-            return redirect("/SwWn#*CW*z/,")
+            return redirect("/mr0rqm9otr8jenter")
     else:
-        return redirect("/HBZT,+FQhi*")
+        return redirect("/apyaig9e2qfxlogin")
 
 # 退会
-@app.route("/zawuvwd5yjlx/<int:id>")
+@app.route("/zawuvwd5yjlxdel/<int:id>")
 def del_trash(id):
     # conn = mysql.connector.connect(user='root',password='A7RmwDLh-Uci',host='127.0.0.1',database='yoshida')
     conn = mysql.connector.connect(user='be0df0a2c105ea',password='71cdc06b',host='us-cdbr-iron-east-04.cleardb.net',database='heroku_c42eab1ee628ff3')
@@ -166,9 +166,9 @@ def del_trash(id):
     c.execute("delete from users where id = %s" , (id,))
     conn.commit()
     conn.close()
-    return redirect("/B8)$h9r|Zh6w")
+    return redirect("/u2rdvj15g8q6owner")
 
-@app.route("/d7sigji69pq6", methods=["POST"])
+@app.route("/d7sigji69pq6regist", methods=["POST"])
 def regist():
         name = request.form.get("name")
         salt = pass_gen()
@@ -179,16 +179,16 @@ def regist():
         c.execute("insert into users values(null,%s,%s,0,%s)",(name,password,salt))
         conn.commit()
         conn.close()
-        return redirect("/B8)$h9r|Zh6w")
+        return redirect("/u2rdvj15g8q6owner")
 
-@app.route("/fi3hpxadupva")
+@app.route("/fi3hpxadupvaownerregist")
 def ownerregist():
         return render_template("ownerregist.html")
 
-@app.route("/dh4vsbhfanj8")
+@app.route("/dh4vsbhfanj8logout")
 def logout():
     session.pop("user_id" ,None)
-    return redirect("/HBZT,+FQhi*")
+    return redirect("/apyaig9e2qfxlogin")
 
 if __name__ == "__main__":
     app.run()
